d98f3743d325f7d8e2f430f77b7d8e37fcdf0c75
Valeriya Yemets <valerakod30@gmail.com>
2023-07-06 23:36:14 +0100

    Проектная работа 14

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const { constants } = require('node:http2');
 const express = require('express');
 const mongoose = require('mongoose');
+const NotFoundError = require('./errors/NotFoundError');
 
 const userRoutes = require('./routes/user');
 const cardRoutes = require('./routes/card');
-const { login, createUser } = require('./controllers/user');
+const { login, createNewUser } = require('./controllers/user');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,12 +17,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb').then(() => {
 
 app.use(userRoutes);
 app.use(cardRoutes);
-app.all('*', (req, res) => {
-  res
-    .status(constants.HTTP_STATUS_NOT_FOUND)
-    .send({ message: 'Route not found' });
+app.all('*', (req, res, next) => {
+  next(new NotFoundError('Route not found'));
 });
 
 app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', createNewUser);
 app.listen(PORT);
